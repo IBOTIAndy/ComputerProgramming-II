3c329d02dc0a382f187b1f6176ae19a088c775fe
IBOTIAndy_PC_NTp <visua64@gmail.com>
2019-03-02 00:42:14 +0800

    矩陣運算_Matrix Operation 完成 2019/03/02_AM.00:40

--- 002/main.c
+++ 002/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 //002 矩陣運算 Matrix_Operation
-//2019/03/01 IBOTIAndy
+//2019/03/02 IBOTIAndy A.M. 00:40
 
 //-----input-----
 void setMatrix(int *row, int *col, int matrix[][10]){   //輸入矩陣
@@ -40,13 +40,13 @@ void output(int row, int col, int ans[][10]){
 //-----run-----
 int matrixMultiply(int y, int x, int n, int matrix1[][10], int matrix2[][10]){
     int i=0, ans=0;
-    for(i=1; i <= n; i++){
+    for(i=0; i < n; i++){
         ans = ans + matrix1[y][i] * matrix2[i][x];
     }
     return ans;
 }
 
-void matrixMath(int type, int row, int col, int matrix1[][10], int matrix2[][10], int matrixans[][10]){
+void matrixMath(int type, int row, int col, int n, int matrix1[][10], int matrix2[][10], int matrixans[][10]){
     int y=0, x=0;
     for(y=0; y < row; y++){
         for(x=0; x < col; x++){
@@ -56,9 +56,9 @@ void matrixMath(int type, int row, int col, int matrix1[][10], int matrix2[][10]
             else if(type == 1){
                 matrixans[y][x] = matrix1[y][x] - matrix2[y][x];
             }
-            //else if(type == 2){
-            //    matrixans[y][x] = matrixMultiply(y, x, col, matrix1, matrix2);
-            //}
+            else if(type == 2){
+                matrixans[y][x] = matrixMultiply(y, x, n, matrix1, matrix2);
+            }
         }
     }
 }
@@ -67,11 +67,11 @@ void run(int row1, int col1, int matrix1[][10], int row2, int col2, int matrix2[
     int addans[10][10]={{0}};
     int subans[10][10]={{0}};
     int mulans[10][10]={{0}};
-    int error=0;
+    int error=0, n=0;
     error = 12202;
     if(row1 == row2 && col1 == col2){
-        matrixMath(0, row1, col1, matrix1, matrix2, addans);
-        matrixMath(1, row1, col1, matrix1, matrix2, subans);
+        matrixMath(0, row1, col1, n, matrix1, matrix2, addans);
+        matrixMath(1, row1, col1, n, matrix1, matrix2, subans);
         output(row1, col1, addans);
         output(row1, col1, subans);
     }
@@ -80,8 +80,9 @@ void run(int row1, int col1, int matrix1[][10], int row2, int col2, int matrix2[
         output(error, error, subans);
     }
     if(col1 == row2){
-        matrixMath(2, row2, col1, matrix1, matrix2, subans);
-        output(row2, col1, mulans);
+        n = col1;   //or "n = row2" 紀錄要重複幾次乘法，因為例如2x3 3x2 結果會是2x2但是計算時會3次乘法相加
+        matrixMath(2, row1, col2, n, matrix1, matrix2, mulans);
+        output(row1, col2, mulans);
     }
     else{
         output(error, error, mulans);
@@ -113,13 +114,13 @@ void testinput(int *row1, int *col1, int matrix1[][10], int *row2, int *col2, in
 //    matrix2[2][0] = 8; matrix2[2][1] = 6; matrix2[2][2] = 3;
 //    matrix2[3][0] = 7; matrix2[3][1] = 4; matrix2[3][2] = 2;
     //乘法測資
-    *col1 = *row2 = 2;
-    *row1 = 2;
+    *row1 = *col1 = 2;
+    *row2 = 2;
     *col2 = 3;
-    matrix1[0][0] = 2; matrix1[0][1] = 1;
-    matrix1[1][0] = 5; matrix1[1][1] = 3;
-    matrix2[0][0] = 8; matrix2[0][1] = 5; matrix2[0][2] = 7;
-    matrix2[1][0] = 7; matrix2[1][1] = 5; matrix2[1][2] = 3;
+    matrix1[0][0] = 1; matrix1[0][1] = 3;
+    matrix1[1][0] = 2; matrix1[1][1] = 0;
+    matrix2[0][0] = 5; matrix2[0][1] = 0; matrix2[0][2] = 1;
+    matrix2[1][0] = 3; matrix2[1][1] = -2; matrix2[1][2] = 6;
     //output(*row1, *col1, matrix1);
     //output(*row2, *col2, matrix2);
 }
@@ -132,8 +133,8 @@ int main(){
     int matrix2[10][10]={{0}};      //矩陣2
     int row2=0, col2=0;             //(行 與 列)2
     //outputM(matrix2);               //輸出矩陣2 #test
-    //testinput(&row1, &col1, matrix1, &row2, &col2, matrix2);    //測試用資料，須將輸入註解
     input(&row1, &col1, matrix1, &row2, &col2, matrix2);        //輸入矩陣一、二的行列與值
+    //testinput(&row1, &col1, matrix1, &row2, &col2, matrix2);    //測試用資料，須將輸入註解
     run(row1, col1, matrix1, row2, col2, matrix2);  //執行矩陣的加法 type為0
     return 0;
 }
