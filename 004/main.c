e377d01e48b4512a4dfea915b7eda250b3954057
IBOTIAndy_PC_NTp <visua64@gmail.com>
2019-03-05 21:31:27 +0800

    Completed the everyoneGrade_Sum and Leaderboard

--- 004/main.c
+++ 004/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-
-//004 計算成績
+#include <string.h>
+//004 計算總成績、平均、排名
 //2019/03/05 IBOTIAndy
 typedef struct{
     int ID;
@@ -8,16 +8,76 @@ typedef struct{
     int grade1;
     int grade2;
     int grade3;
-    int gradeadd;
+    int gradesum;
+    int rank;
 }student;
 
+//-----input-----
 void input(student students[], int *n){
     int i=0;
     scanf("%d", n);
     for(i=0; i < *n; i++){
-        scanf("%d %s %d %d %d", &students[i].ID, students[i].name, &students[i].grade1, &students[i].grade2, &students[i].grade3);
+        scanf("%d", &students[i].ID);
+        scanf("%s", students[i].name);
+        scanf("%d", &students[i].grade1);
+        scanf("%d", &students[i].grade2);
+        scanf("%d", &students[i].grade3);
     }
 }
+//----/input-----
+
+//-----output-----
+void outputLeaderboard(student students[], int n){
+    int rank=0, j=0;
+    for(rank=1; rank <= n; rank++){
+        for(j=0; j < n; j++){
+            if(students[j].rank == rank){
+                printf("%d ", students[j].ID);
+                printf("%s ", students[j].name);
+                printf("%d ", students[j].grade1);
+                printf("%d ", students[j].grade2);
+                printf("%d ", students[j].grade3);
+                printf("%d ", students[j].gradesum);
+                printf("%d ", students[j].rank);
+                printf("\n");
+                j = n;
+            }
+        }
+    }
+}
+//----/output-----
+
+//-----gradeMath-----
+void everybodyGradesum(student students[], int n){
+    int i=0;
+    for(i=0; i < n; i++){
+        students[i].gradesum = students[i].grade1 + students[i].grade2 + students[i].grade3;
+    }
+}
+void ranking(student students[], int n){
+    int i=0, j=0, first=0, firstGradesum=0;
+    for(i=0; i < n; i++){
+        first = 0;
+        firstGradesum = 0;
+        for(j=0; j < n; j++){
+            if(students[j].rank == 0){
+                if(students[j].gradesum >= firstGradesum){
+                    firstGradesum = students[j].gradesum;
+                    first = j;
+                }
+            }
+        }
+        students[first].rank = i + 1;
+    }
+}
+
+void gradeMath(student students[], int n){
+    everybodyGradesum(students, n);
+    ranking(students, n);
+    outputLeaderboard(students, n);
+}
+//----/gradeMath-----
+
 //-----test-----
 void testInput(student students[], int *n){
     *n=5;
@@ -35,19 +95,17 @@ void testInput(student students[], int *n){
 void inputDataOutput(student students[], int n){
     int i=0;
     for(i=0; i < n; i++){
-        printf("%d %s %d %d %d %d\n", students[i].ID, students[i].name, students[i].grade1, students[i].grade2, students[i].grade3, students[i].gradeadd);
+        printf("%d %s %d %d %d %d\n", students[i].ID, students[i].name, students[i].grade1, students[i].grade2, students[i].grade3, students[i].gradesum);
     }
 }
-
 //----/test-----
-int main()
-{
+int main(){
     int n=0;
-    student students[100]={{0, "", 0, 0, 0, 0}};
-    input(students, &n);
-    //testInput(students, &n);
-    //inputDataOutput(students, n);
-//    gradeMath(student, n);
+    student students[100]={{0, "", 0, 0, 0, 0, 0}};
+    //input(students, &n);
+    testInput(students, &n);
+    inputDataOutput(students, n);
+    gradeMath(students, n);
 //    printf("Hello world!\n");
     return 0;
 }
