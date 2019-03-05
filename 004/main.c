eb66e30f4ef17fa39af5cb184baf1b0a89295ba1
IBOTIAndy_PC_NTp <visua64@gmail.com>
2019-03-05 13:18:49 +0800

    004 計算成績 第一次上傳: 大概建立了書寫方向 已完成struct

---
+++ 004/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+//004 ­pºâ¦¨ÁZ
+//2019/03/05 IBOTIAndy
+typedef struct{
+    int ID;
+    char name[10];
+    int grade1;
+    int grade2;
+    int grade3;
+    int gradeadd;
+}student;
+
+
+int main()
+{
+    int n=0;
+    student students[100]={{0,"",0,0,0,0}};
+//    input(student, &n);
+//    gradeMath(student, n);
+//    printf("Hello world!\n");
+    return 0;
+}
