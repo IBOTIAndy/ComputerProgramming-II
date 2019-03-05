478aa7df409ebd3588713d5f1ac32533b01f26af
IBOTIAndy_PC_NTp <visua64@gmail.com>
2019-03-06 00:17:19 +0800

    Completed the sum_of_more_then_Median(大於中位數的總成績的平均) and sum_of_lest_then_Median(小於中位數的總成績的平均) repair n is even, Median forget Divide by 2(修正當總數為偶數時，中位數忘記除以2)

--- 004/main.c
+++ 004/main.c
@@ -112,6 +112,7 @@ float gradesumMedian(student students[], int n){
                 ans = ans + students[i].gradesum;
             }
         }
+        ans = ans / 2;
     }
     else{
         for(i=0; i < n; i++){
@@ -137,24 +138,40 @@ float gradesumSD(student students[], float sumVariance, int n){
     ans = sqrt(sumVariance);
     return floatroundingdot2(ans);
 }
+void gradesAverageofmorelestthenMedian(student students[], float sumMedian, int n){
+    int i=0;
+    int moren=0, lestn=0;
+    float morethenans=0, lestthenans=0;
+    for(i=0; i < n; i++){
+        if(students[i].gradesum > sumMedian){
+            morethenans = morethenans + students[i].gradesum;
+            moren = moren + 1;
+        }
+        else if(students[i].gradesum < sumMedian){
+            lestthenans = lestthenans + students[i].gradesum;
+            lestn = lestn + 1;
+        }
+    }
+    outputFloatGrade(floatroundingdot2(morethenans / moren));
+    outputFloatGrade(floatroundingdot2(lestthenans / lestn));
+}
 
 void gradeMath(student students[], int n){
     float sumAverage=0;
     float sumMedian=0;
     float sumVariance=0;
     float sumSD=0;
-    float morethenMediansum=0;
-    float lessthenMediansum=0;
-    everyoneGradesum(students, n);                      //計算每個人的總成績
-    ranking(students, n);                               //計算名次
-    outputLeaderboard(students, n);                     //依照排名輸出
-    sumAverage = gradesAverage(students, n);              //計算每個成績的平均
-    sumMedian = gradesumMedian(students, n);                        //Median 中位數
-    sumVariance = gradesumVariance(students, sumAverage, n);                      //Variance 變異數
-    sumSD = gradesumSD(students, sumVariance, n);
+    everyoneGradesum(students, n);                              //計算每個人的總成績
+    ranking(students, n);                                       //計算名次
+    outputLeaderboard(students, n);                             //依照排名輸出
+    sumAverage = gradesAverage(students, n);                    //計算每個成績的平均
+    sumMedian = gradesumMedian(students, n);                    //Median 中位數
+    sumVariance = gradesumVariance(students, sumAverage, n);    //Variance 變異數
+    sumSD = gradesumSD(students, sumVariance, n);               //SD = Standard Deviation 標準差
     outputFloatGrade(sumMedian);
     outputFloatGrade(sumVariance);
-    outputFloatGrade(sumSD);          //SD = Standard Deviation 標準差
+    outputFloatGrade(sumSD);
+    gradesAverageofmorelestthenMedian(students, sumMedian, n);
 }
 //----/gradeMath-----
 
@@ -182,9 +199,9 @@ void inputDataOutput(student students[], int n){
 int main(){
     int n=0;
     student students[100]={{0, "", 0, 0, 0, 0, 0}};
-    //input(students, &n);
-    testInput(students, &n);
-    inputDataOutput(students, n);
+    input(students, &n);
+    //testInput(students, &n);
+    //inputDataOutput(students, n);
     gradeMath(students, n);
 //    printf("Hello world!\n");
     return 0;
