c2c4043e7c9c31d0119a9cfac149f2a7a21e8cc9
IBOTIAndy_PC_NTp <visua64@gmail.com>
2019-03-09 20:41:49 +0800

    Completed the runloop(重複輸入執行) and the gradeMath(計算成績) and the creditSum(學分總和) and gradeAverage(總平均) and ranking(排名)

--- 005/main.c
+++ 005/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 //005 查詢學生學期成績單
-//2019/03/09_PM.06:26
+//2019/03/09_PM.08:39
 typedef struct{
     char name[20];
     int credit;
@@ -13,6 +13,9 @@ typedef struct{
     char name[10];
     int course;
     course courses[10];
+    int creditsum;
+    float gradeaverage;
+    int rank;
 }student;
 
 //-----input-----
@@ -35,16 +38,16 @@ void inputStudent(student *students){
     }
 }
 
-void input(student students[]){
-    int i=0, n=0;
-    scanf("%d", &n);
-    for(i=0; i < n; i++){
+void input(student students[], int *n){
+    int i=0;
+    scanf("%d", n);
+    for(i=0; i < *n; i++){
         inputStudent(students + i);
     }
 }
 //----/input-----
 
-//-----output-----
+//-----intputDataOutput-----
 void inputDataOutputCourse(course *courses){
     printf("    %s %d %d\n", courses->name, courses->credit, courses->grade);
 }
@@ -58,6 +61,9 @@ void inputDataOutputStudent(student *students){
     for(i=0; i < students->course; i++){
         inputDataOutputCourse(&students->courses[i]);
     }
+    printf("總學分: %d\n", students->creditsum);
+    printf("總分: %.2f\n", students->gradeaverage);
+    printf("排名: %d\n", students->rank);
     printf("\n");
 }
 
@@ -67,17 +73,99 @@ void inputDataOutput(student students[], int n){
         inputDataOutputStudent(&students[i]);
     }
 }
+//----/intputDataOutput-----
+
+//-----output-----
+void output(student students[], int i){
+    i = i + 1;
+}
 //----/output-----
+
+//-----main-----
+void creditSum(student *students){
+    int i=0, n=0, ans=0;
+    n = students->course;
+    for(i=0; i < n; i++){
+        ans = ans + students->courses[i].credit;
+    }
+    students->creditsum = ans;
+}
+void gradeAverage(student *students){
+    int i=0, n=0;
+    float ans=0.0;
+    n = students->course;
+    for(i=0; i < n; i++){
+        ans = ans + (students->courses[i].credit * students->courses[i].grade);
+    }
+    ans = ans / students->creditsum;
+    students->gradeaverage = ans;
+}
+
+void ranking(student students[], int n){
+    int i=0, j=0, first=0;
+    float firstGradesum=0.0;
+    for(i=0; i < n; i++){
+        first = 0;
+        firstGradesum = 0;
+        for(j=0; j < n; j++){
+            if(students[j].rank == 0){
+                if(students[j].gradeaverage >= firstGradesum){
+                    firstGradesum = students[j].gradeaverage;
+                    first = j;
+                }
+            }
+        }
+        students[first].rank = i + 1;
+    }
+}
+
+void gradeMath(student students[], int n){
+    int i=0;
+    for(i=0; i < n; i++){
+        creditSum(&(students[i]));
+        gradeAverage(&(students[i]));
+    }
+    ranking(students, n);
+}
+
+int selectID(student students[], int inID, int *i, int n){
+    *i = 0;
+    while(*i < n){
+        if(students[*i].ID == inID){
+            return 1;
+        }
+        *i = *i + 1;
+    }
+    return 0;
+}
+
+void run(student students[], int n){
+    int in=0, i=0;
+    scanf("%d", &in);
+    gradeMath(students, n);
+    while(in != 0){
+        if(selectID(students, in, &i, n)){
+            output(students, i);
+        }
+        else{
+            printf("error\n");
+        }
+        scanf("%d", &in);
+    }
+}
+//----/main-----
 
 //-----test-----
 
 //----/test-----
 
 int main(){
-    student students[10];
-    input(students);
-    inputDataOutput(students, 3);
-    //run();
+    int n;
+    student students[10]={{0, "", 0, {}, 0, 0, 0}};
+    input(students, &n);
+    //inputDataOutput(students, 3);
+    run(students, n);
+    //inputDataOutput(students, 3);
 //    printf("Hello world!\n");
     return 0;
 }
