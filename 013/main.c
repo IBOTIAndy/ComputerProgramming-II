13f0768dc14f1658a1119a8b436e74e381c39c77
IBOTIAndy <visua64@gmail.com>
2019-03-29 23:42:06 +0800

    Complete the test_outputmap and inputFruits and mapReset(測試用輸出地圖、輸入水果座標、地圖重置)

--- 013/main.c
+++ 013/main.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
 
 //013 貪食蛇
-//2019/03/28 AM.00:35 IBOTIAndy
-typedef struct snake{
-    int body[2];
-    struct snake *tail;
+//2019/03/29 PM.11:09 IBOTIAndy
+typedef struct snake{   //蛇的結構，會往尾巴的方向指
+    int body[2];        //目前位置的座標
+    struct snake *tail; //往尾巴的方向指
 }snake;
+
+//----------test----------
+void testOutputMap(char map[20][20], int n){    //輸出整張地圖 (空地與水果位置)
+    int x=0, y=0;
+    for(x=0; x < n; x++){
+        for(y=0; y < n; y++){
+            printf("%c ", map[x][y]);
+        }
+        printf("\n");
+    }
+}
+//---------/test----------
 
 //----------run----------
 
 //-----input-----
-void inputFruits(char map[20][20]){}
+void inputFruits(char map[20][20]){ //輸入水果的位置
+    int i=0, n=0, x=0, y=0;         //
+    scanf("%d", &n);                //輸入水果數量 (0 ~ 10)
+    for(i=0; i < n; i++){           //直到所有水果都輸入了
+        scanf("%d %d", &x, &y);     //輸入水果的座標，左上角為 (0,0) (x,y)
+        map[x][y] = '#';            //'#' 代表水果
+    }
+}
 //----/input-----
 
 //-----output-----
@@ -18,15 +37,29 @@ void printSnake(snake *head){}
 //----/output-----
 
 //-----map-----
-void mapReset(char map[20][20]){}
-
-void mapUpdate(char map[20][20], snake *head){}
+void mapReset(char map[20][20]){        //地圖重置
+    int x=0, y=0, n=0;
+    n = 20;
+    for(x=0; x < n; x++){               //掃描整個地圖(x)
+        for(y=0; y < n; y++){           //掃描整個地圖(y)
+            if(x == 0 || x == 19){      //如果是在邊線
+                map[x][y] = '1';        //設定成牆壁
+            }
+            else if(y == 0 || y == 19){ //如果是在邊線
+                map[x][y] = '1';        //設定成牆壁
+            }
+            else{                       //如果不是邊線
+                map[x][y] = '0';        //設定為空地
+            }
+        }
+    }
+}
 //----/map-----
 
 //-----game-----
-int isGameOver(char map[20][20], snake *head, int next){}
+int isGameOver(char map[20][20], snake *head, int next){return 0;}
 
-int eatFruit(char map[20][20], snake *head, int next){}
+int eatFruit(char map[20][20], snake *head, int next){return 0;}
 
 void growingUp(snake *head){}
 
@@ -35,12 +68,13 @@ void move(snake *head){}
 
 void run(char map[20][20], snake *head){
     int next=0;
-    next = 4;           //初始方向為右
-    mapReset(map);
-    inputFruits(map);
-    scanf("%d", &next);
+    next = 4;               //初始方向為右
+    mapReset(map);          //重置地圖
+    testOutputMap(map, 20); //輸出地圖 (測試用)
+    inputFruits(map);       //輸入水果
+    testOutputMap(map, 20); //輸出地圖 (測試用)
+    scanf("%d", &next);     //第一次輸入
     while(next != -1){                      //如果直接結束
-        mapUpdate(map, head);               //更新地圖
         if(next == 0){                      //印出蛇的長度與座標
             printSnake(head);
         }
@@ -60,7 +94,7 @@ void run(char map[20][20], snake *head){
 
 int main(){
     snake head={{0}, NULL};
-    char map[20][20]={'0'};
+    char map[20][20]={{' '}};
     run(map, &head);
     return 0;
 }
