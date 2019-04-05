cdb085d93eb42c788e79bfbc6f3b17e91820abd5
IBOTIAndy_PC_NTp <visua64@gmail.com>
2019-04-05 14:55:23 +0800

    015_老鼠走迷宮 已完成基本架構

---
+++ 015/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+//015 ¦Ñ¹«¨«°g®c
+//2019/04/05 IBOTIAndy PM. 02:00
+
+//----------input----------
+void input(char map[100][100], int *n){}
+//---------/input----------
+
+//----------run----------
+void run(char map[100][100], int n){}
+//---------/run----------
+
+
+//----------output----------
+void output(char map[100][100], int n){}
+//---------/output----------
+
+
+int main(){
+    char map[100][100]={{'0'}};
+    int n=0;
+    input(map, &n);
+    run(map, n);
+    output(map, n);
+//    printf("Hello world!\n");
+    return 0;
+}
