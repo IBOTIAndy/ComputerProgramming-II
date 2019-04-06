e74d91e1ee876367cb4914c2a6604f2b5f8a28bf
IBOTIAndy_PC_NTp <visua64@gmail.com>
2019-04-06 15:51:58 +0800

    set_magic_block is completed(完成設定魔術方塊)

--- 016/main.c
+++ 016/main.c
@@ -10,9 +10,53 @@ typedef struct{
 //}magicBlock;
 
 //----------set----------
-void setSide(side *blockSide, int i){}
+//-----setTest-----
+void setTestOutput(side magicBlock[]){  //確認輸出正不正確
+    int side=0, i=0, j=0;
+    for(side=0; side < 6; side++){
+        printf("\nSide%d:\n", side + 1);
+        for(i=0; i < 3; i++){
+            for(j=0; j < 3; j++){
+                printf("%d ", magicBlock[side].data[i][j]);
+            }
+            printf("\n");
+        }
+    }
+}
+//----/setTest-----
+void reverseSide(side *side5){              //改變背面的設定
+    int temporarily[3]={0};
+    temporarily[0] = side5->data[0][0];
+    temporarily[1] = side5->data[0][1];
+    temporarily[2] = side5->data[0][2];
+    side5->data[0][0] = side5->data[2][0];
+    side5->data[0][1] = side5->data[2][1];
+    side5->data[0][2] = side5->data[2][2];
+    side5->data[2][0] = temporarily[0];
+    side5->data[2][1] = temporarily[1];
+    side5->data[2][2] = temporarily[2];
+}
 
-void setMagicBlock(side magicBlock[]){}
+void setSide(side *blockSide, int side){        //設定一個面
+    int i=0, j=0, n=0, x=0;
+    n = 3;
+    side = side * 10;                           //代表的面
+    for(i=0; i < n; i++){
+        for(j=0; j < n; j++){
+            blockSide->data[i][j] = side + x;   //
+            x = x + 1;
+        }
+    }
+}
+
+void setMagicBlock(side magicBlock[]){  //設定魔術方塊
+    int i=0, n=0;
+    n = 6;                              //方塊為6面
+    for(i=0; i < n; i++){
+        setSide(&magicBlock[i], i + 1); //每次設定一個面
+    }
+    reverseSide(&magicBlock[4]);        //背面特殊排列
+}
 //---------/set----------
 
 
@@ -51,6 +95,7 @@ void run(side magicBlock[]){
 int main(){
     side magicBlock[6];
     setMagicBlock(magicBlock);
+//    setTestOutput(magicBlock);          //測試有沒有正確寫入
     run(magicBlock);
 //    printf("Hello world!\n");
     return 0;
