d5f759a46d064fefabbcbaa876a943d0e4bfeab3
IBOTIAndy_PC_NTp <visua64@gmail.com>
2019-04-30 01:30:17 +0800

    win_how_much is completed. (連了多少線 完成)

--- 020/main.c
+++ 020/main.c
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <string.h>
 //020 賓果遊戲
-//2019/04/30 AM.00:42 IBOTIAndy
+//2019/04/30 AM.01:29 IBOTIAndy
 typedef struct{
     int bingoBoard[500][500];
     int x;
@@ -67,6 +67,59 @@ void findSelectNumber(bingoGame *player, int select){
     }
 }
 
+void rowWin(bingoGame *player, int y){
+    int x=0, connection=0;
+    connection = 1;
+    for(x=0; x < player->x; x++){
+        if(player->bingoBoard[y][x] != -1){
+            connection = 0;
+            break;
+        }
+    }
+    player->winLine = player->winLine + connection;
+}
+
+void columnWin(bingoGame *player, int x){
+    int y=0, connection=0;
+    connection = 1;
+    for(y=0; y < player->y; y++){
+        if(player->bingoBoard[y][x] != -1){
+            connection = 0;
+            break;
+        }
+    }
+    player->winLine = player->winLine + connection;
+}
+
+void slashWin(bingoGame *player){
+    int i=0, x=0, y=0, connection=0;
+    connection = 2;
+    for(i=0; i < player->x; i++){
+        if(player->bingoBoard[i][i] != -1){
+            connection = connection - 1;
+            break;
+        }
+    }
+    x = player->x - 1;
+    for(y=0; y < player->y; y++, x--){
+        if(player->bingoBoard[y][x] != -1){
+            connection = connection - 1;
+            break;
+        }
+    }
+    player->winLine = player->winLine + connection;
+}
+
+void winHowMuch(bingoGame *player){
+    int i=0;
+    for(i=0; i < player->x; i++){
+        rowWin(player, i);
+        columnWin(player, i);
+    }
+    slashWin(player);
+    //斜線
+}
+
 void run(bingoGame *player1, bingoGame *player2){
     int select=0;
     char *selectNumber;
@@ -74,13 +127,16 @@ void run(bingoGame *player1, bingoGame *player2){
     scanf("%c", &selectLine[0]);
     gets(selectLine);
     selectNumber = strtok(selectLine, " ");
-    select = changeType(selectNumber);
-    while(selectNumber[0] != '\0'){
+    while(selectNumber != NULL){
+        select = changeType(selectNumber);
         findSelectNumber(player1, select);          //圈出輸入的數字
         findSelectNumber(player2, select);          //圈出輸入的數字
         selectNumber = strtok(NULL, " ");
-        select = changeType(selectNumber);
     }
+    winHowMuch(player1);
+    winHowMuch(player2);
+//    printf("player1=%d\n", player1->winLine);
+//    printf("player2=%d\n", player2->winLine);
 }
 
 int main(){
