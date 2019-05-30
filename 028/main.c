a1781f1dcba63e129430a770d5a7186019c13ac3
IBOTIAndy <visua64@gmail.com>
2019-05-30 14:59:45 +0800

    Create_queue is completed. (完成建立queue) README.md is update. (更新 README.md 的排版)

--- 028/main.c
+++ 028/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 //028 Linked list Queue實作
-//2019/05/29 PM. 01:21 IBOTIAndy
+//2019/05/30 PM. 02:53 IBOTIAndy
 //----------typedef----------
 typedef struct element_s{   //元素結構
     int data;               //元素
@@ -21,13 +21,46 @@ typedef struct queueList_s{ //隊伍的串列
 }queueList_t;
 //---------/typedef----------
 
+//----------
+queue_t* createNewQueue(char *name){    //建立新 queue 並回傳
+    queue_t *newQueue=(queue_t*)malloc(sizeof(queue_t));    //給予新的 queue 空間
+    strcpy(newQueue->name, name);   //設定名稱
+    newQueue->root=NULL;            //檔案指向 NULL
+    newQueue->size=0;               //檔案大小初始值
+    newQueue->next=NULL;            //下一個 queue 為 NULL
+    return newQueue;                //回傳新的 queue 記憶體位置
+}
+
 //----------run--------------
+//-------createQueue-------
+queue_t* setListTail(queue_t *root, queue_t *newQueue){    //設定 queue 串列
+    queue_t *currently=NULL;
+    if(root == NULL){                   //如果沒有任何的 queue
+        return newQueue;                //回傳新的 queue 位置
+    }
+    currently = root;                   //目前指向第一個
+    while(currently->next != NULL){     //如果下一個位置不是空的
+        currently = currently->next;    //往下走
+    }                                   //直到最後一個
+    currently->next = newQueue;         //將新的 queue 放在最後面
+    return root;                        //回傳開頭
+}
+
+void createQueue(queueList_t *queueList){   //建立 queue
+    char name[20]="";   //queue 的名稱
+    queue_t *newQueue=NULL;             //新增一個 queue 指標
+    gets(name);                         //輸入名稱
+    newQueue = createNewQueue(name);    //建立 queue 並回傳記憶體位置
+    queueList->root = setListTail(queueList->root, newQueue);   //將新的 queue 放入串列的尾部
+}
+//------/createQueue-------
+
 void run(queueList_t *queueList){
     char in[2]="";
     while(1){
         gets(in);
         if(!strcmp(in, "1")){       //Create_queue
-//            createQueue();
+            createQueue(queueList); //建立一個 queue
         }
         else if(!strcmp(in, "2")){  //Enter_queue_data
 //            enterQueueData();
